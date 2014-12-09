1fc70d4e11b0c6215d909fa1a3c850e624bda815
Owtopsy <donia_b_isn@hotmail.fr>
2014-12-09 23:55:50 +0100

    Add Editor "ToolCharacters"

---
+++ Nope/Assets/Editor/toolCharacters.cs
@@ -0,0 +1,204 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+
+
+public class toolCharacters : EditorWindow{
+    string className = "class name";
+    string hptext ;
+    string mptext;
+    string strenghtext;
+    string armortext;
+    string attackRangetext;
+    string mobilityRangetext;
+    string moveSpeedtext;
+    int hp;
+    int mp;
+    int strengh;
+    int armor;
+    int attackRange;
+    int mobilityRange;
+    int moveSpeed;
+    public static UnityEngine.Object emptyObj;
+    string [] specifType = { "Tenacité", "Détection des piéges", "Téléportation" };
+    int [] indexScriptTab = {0,0,0,0,0,0,0,0};
+    string [] scriptTab;
+    string [] spriteTab;
+    string [] previewSprite;
+    int idScript = 0;
+	int index = 0;
+    int specifIndex = 0;
+    int scriptIndex = 0;
+    int spriteIndex = 0;
+    public CharactersAttributes attributes;
+    Editor gameObjectEditor;
+    GameObject newClass;
+    Sprite subSprite;
+    Sprite mySprite;
+
+    
+    [MenuItem ("Window/tool characters")]
+
+    public static void showWindow()
+    {
+        EditorWindow.GetWindow(typeof(toolCharacters));
+
+    }
+
+    void OnGUI()
+    {
+        /*** add scripts in path to the tab and get only scripts names***/
+        /* can choose any scripts */
+        string [] Tab = Directory.GetFiles("Assets/Scripts", "*.cs");
+        scriptTab = new string[Tab.Length + 1];
+        scriptTab[0] = "aucun";
+        for (int j = 0; j < Tab.Length; j++ )
+        {
+                Tab[j] = Tab[j].Split('\\')[1].Split('.')[0];
+                scriptTab[j + 1] = Tab[j];
+        }
+
+
+        /*** add Sprite and Sprite's images in Tab and get only Sprites names***/
+        spriteTab = Directory.GetFiles("Assets/Resources/Sprites");
+        previewSprite = Directory.GetFiles("Assets/Resources/Images");
+        for (int j = 0; j < spriteTab.Length; j++)
+        {
+            spriteTab[j] = spriteTab[j].Split('\\')[1].Split('.')[0];
+            previewSprite[j] = previewSprite[j].Split('\\')[1].Split('.')[0];      
+        }
+       
+          
+        /*** Choose class Name ***/
+        EditorGUILayout.LabelField("Characters'Editor", EditorStyles.boldLabel);
+        className = EditorGUILayout.TextField("Enter name class", className);
+        EditorGUILayout.BeginHorizontal();
+        //int i = index;
+
+        /*** choose Sprite and Preview it ***/
+        spriteIndex = EditorGUILayout.Popup(spriteIndex, spriteTab);
+        Sprite mypreviewSprite = Resources.Load<Sprite>("Images/" + previewSprite[spriteIndex]);
+        
+
+        /*** Preview ***/
+        Rect area = GUILayoutUtility.GetRect(60, 80);
+        area.width = 100;
+        EditorGUI.DrawPreviewTexture(new Rect(area), mypreviewSprite.texture);
+        EditorGUILayout.EndHorizontal();
+
+        /*** character 's parameter ***/
+        hptext = EditorGUILayout.TextField("hp",hptext);
+        mptext = EditorGUILayout.TextField("mp", mptext);
+        strenghtext = EditorGUILayout.TextField("Strengh", strenghtext);
+        armortext = EditorGUILayout.TextField("Armor", armortext);
+        attackRangetext = EditorGUILayout.TextField("Attack Range", attackRangetext);
+        mobilityRangetext = EditorGUILayout.TextField("Mobility Range", mobilityRangetext);
+        moveSpeedtext = EditorGUILayout.TextField("Attaque Range", moveSpeedtext);
+        specifIndex = EditorGUILayout.Popup(specifIndex, specifType);
+
+        /*** can choose script in path to add at the gameobject or any script ***/
+        indexScriptTab[0] = EditorGUILayout.Popup(indexScriptTab[0], scriptTab);
+        for (int s = 1; s < indexScriptTab.Length; s++)
+        {
+
+            if (indexScriptTab[s-1] != 0 )
+            {
+                indexScriptTab[s] = EditorGUILayout.Popup(indexScriptTab[s], scriptTab);
+            }
+        }
+
+        /*** CREATE BUTTON ***/
+        if (GUILayout.Button("Create")) {
+
+            /***  class Name  ***/
+            string fileName = className;
+            string fileLocation = "Assets/Resources/Prefabs/" + fileName + ".prefab";
+
+            /*** create prefab  ***/
+            emptyObj = PrefabUtility.CreateEmptyPrefab(fileLocation);
+            newClass = new GameObject();
+            newClass.AddComponent<SpriteRenderer>();
+            mySprite = Resources.Load<Sprite>("Sprites/" + spriteTab[spriteIndex]);
+            newClass.GetComponent<SpriteRenderer>().sprite = mySprite;
+
+            /*** to assign component for newClass***/
+            newClass.AddComponent<CharactersAttributes>();
+            attributes = newClass.GetComponent<CharactersAttributes>();
+         
+            /*** assign choice script ***/
+            for (int s = 0 ; s < scriptTab.Length; s++)
+            {
+                if (indexScriptTab[s] != 0)
+                {
+                    newClass.AddComponent(scriptTab[indexScriptTab[s]]);
+                }
+            }
+            
+            /***  assign hp  ***/
+            hp = int.Parse(hptext);
+            attributes.hp = hp;
+
+            /***  assign mp  ***/
+            mp = int.Parse(mptext);
+            attributes.mp = mp;
+
+            /*** assign Strengh ***/
+            strengh = int.Parse(strenghtext);
+            attributes.strengh = strengh;
+
+            /*** assign Armor ***/
+            armor = int.Parse(armortext);
+            attributes.armor = armor;
+
+            /*** assign Attaque Range ***/
+            attackRange = int.Parse(attackRangetext);
+            attributes.attackRange = attackRange;
+
+            /*** assign Mobility Range ***/
+            mobilityRange = int.Parse(mobilityRangetext);
+            attributes.mobilityRange = mobilityRange;
+
+            /*** assign Move Speed ***/
+            moveSpeed = int.Parse(moveSpeedtext);
+            attributes.moveSpeed = moveSpeed;
+
+            /*** assign specification ***/
+            attributes.specifications = specifType[specifIndex];
+
+            /*** assign gameobject to prefab  ***/
+            PrefabUtility.ReplacePrefab(newClass, emptyObj, ReplacePrefabOptions.ConnectToPrefab);
+            DestroyImmediate(newClass);
+
+         }
+       
+        }
+       
+
+    
+        
+
+    /*        void WeaponTypeScript(GameObject go) {
+                switch (weapIndex) {
+				    case 0:
+                        go.AddComponent<CorpsaCorps>();
+                        return;
+				    case 1:
+					    go.AddComponent<CharactersAttributes>();
+                        return;
+                
+				    case 2:
+					    go.AddComponent<CharactersAttributes>();
+                        return;
+
+				    default:
+
+					    return;
+			    }  
+        }*/
+        
+    }
+	
+     
+
---
+++ Nope/Assets/Scripts/CharactersAttributes.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharactersAttributes : MonoBehaviour {
+
+    [SerializeField]
+    private int _hp = 0;
+    public int hp {
+        get{return _hp;}
+        set{_hp = value;}
+    }
+
+    [SerializeField]
+    private int _mp = 0;
+    public int mp{
+        get{return _mp;}
+        set{_mp = value;}
+    }
+
+    [SerializeField]
+    private int _strengh = 0;
+    public int strengh
+    {
+        get { return _strengh; }
+        set { _strengh = value; }
+    }
+
+    [SerializeField]
+    private int _armor = 0;
+    public int armor
+    {
+        get { return _armor; }
+        set { _armor = value; }
+    }
+
+    [SerializeField]
+    private int _attackRange = 0;
+    public int attackRange
+    {
+        get { return _attackRange; }
+        set { _attackRange = value; }
+    }
+
+    [SerializeField]
+    private string _weaponType = "";
+    public string weaponType {
+        get{return _weaponType;}
+        set{_weaponType = value;}
+    }
+
+    [SerializeField]
+    private string _specifications = "";
+    public string specifications
+    {
+        get { return _specifications; }
+        set { _specifications = value; }
+    }
+    
+    [SerializeField]
+    private int _mobilityRange = 0;
+    public int mobilityRange
+    {
+        get { return _mobilityRange; }
+        set { _mobilityRange = value; }
+    }
+
+    [SerializeField]
+    private int _moveSpeed = 0;
+    public int moveSpeed
+    {
+        get { return _moveSpeed; }
+        set { _moveSpeed = value; }
+    }
+}
+
