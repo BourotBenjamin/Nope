194d0909d65178431bb4eff4105b77eae87fbf83
Benjamin Bourot <bourot.benjamin@gmail.com>
2014-12-13 11:52:34 +0100

    It should work with that

--- Nope/Assets/Scripts/GUIScript.cs
+++ Nope/Assets/Scripts/GUIScript.cs
@@ -8,17 +8,41 @@ public class GUIScript : MonoBehaviour {
 
     private bool positionSet;
     private Vector3 positionOnGame;
-    private Vector2 positionOnScreen;
+    private Vector3 positionOnScreen;
     private SimulateScript selectedPlayer; // TODO set selected player
 
     // Update is called once per frame setReadyToSimulate
 	void Update () 
     {
-        if (selectedPlayer!=null && !positionSet && Input.GetButton("Click"))
+        if(Input.GetMouseButtonDown(0))
         {
-            positionSet = true;
-            positionOnGame = new Vector3(0, 0, 0); // TODO get mousePositionOnGame
-            positionOnScreen = new Vector2(0, 0); // TODO get mousePositionOnScreen
+            if (selectedPlayer!=null && !positionSet)
+            { 
+                positionSet = true;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    hit.collider.renderer.material.color = Color.red;
+                    positionOnGame = hit.point; // TODO get mousePositionOnGame
+                    positionOnScreen = Input.mousePosition; // TODO get mousePositionOnScreen
+                }
+            }
+            else
+            {
+
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if(hit.collider.tag == "Player")
+                    {
+                        hit.collider.renderer.material.color = Color.blue;
+                        selectedPlayer.transform.renderer.material.color = Color.white;
+                        selectedPlayer = hit.collider.GetComponent<SimulateScript>();
+                    }
+                }
+            }
         }
 	}
 
